ffa850ad578cfe33b250f772c0c306e840e0029b
hiromasa79 <hiromasa19790502@gmail.com>
2025-11-08 16:59:54 +0900

    build.shをpip installのみに簡略化

--- build.sh
+++ build.sh
@@ -2,16 +2,5 @@
 # exit on error
 set -o errexit
 
-# 1. Pythonライブラリのインストール
+# Pythonライブラリのインストール
 pip install -r requirements.txt
-
-# 2. Renderの環境に、Google Chromeのヘッドレスモード実行に必要な
-#    システムライブラリをインストールする（apt-getの代わりに、Renderが用意したツールを使う）
-apt-get update && apt-get install -y --no-install-recommends \
-    libnss3 \
-    libdbus-glib-1-2 \
-    libgtk-3-0 \
-    libxss1 \
-    libasound2 \
-    # -- 上記はChromeの実行に必要なライブラリ --
-    # ここでChrome本体のインストールは行わず、Renderが提供するものを利用する
