94f992fe83a5e9b0fcdab09e1af740431619a082
hiromasa79 <hiromasa19790502@gmail.com>
2025-11-08 16:53:12 +0900

    Renderのファイルシステム制約に対応

--- build.sh
+++ build.sh
@@ -2,13 +2,16 @@
 # exit on error
 set -o errexit
 
-# Poetryを使って、requirements.txtからライブラリをインストール
+# 1. Pythonライブラリのインストール
 pip install -r requirements.txt
 
-# Debian/UbuntuベースのRenderサーバーに、
-# Google Chromeと、Seleniumが必要とするライブラリをインストール
-apt-get update
-apt-get install -y libnss3 libdbus-glib-1-2 libgtk-3-0 libxss1 libasound2
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
+# 2. Renderの環境に、Google Chromeのヘッドレスモード実行に必要な
+#    システムライブラリをインストールする（apt-getの代わりに、Renderが用意したツールを使う）
+apt-get update && apt-get install -y --no-install-recommends \
+    libnss3 \
+    libdbus-glib-1-2 \
+    libgtk-3-0 \
+    libxss1 \
+    libasound2 \
+    # -- 上記はChromeの実行に必要なライブラリ --
+    # ここでChrome本体のインストールは行わず、Renderが提供するものを利用する
